db9ef3c2ae1a78e8a96f682aacc7bf743341eb69
Bart Wiegmans <bartwiegmans@gmail.com>
2013-09-10 18:22:13 +0200

    moved back to  two-file layout, will split into more files as necessary

--- format.go
+++ format.go
@@ -1,4 +1,4 @@
-package bois
+package main
 
 import (
 	"code.google.com/p/graphics-go/graphics"
@@ -8,18 +8,15 @@ import (
 	"image/draw"
 	"image/jpeg"
 	"image/png"
-	"io"
 	"regexp"
 	"strconv"
+	"io"
 )
 
-var SuffixRegexp = regexp.MustCompile("\\.((q\\d+\\.)?jpe?g|png)$")
-var ScaleRegexp = regexp.MustCompile("^(scale-)?(\\d+)x(\\d+)$")
-var ClipRegexp = regexp.MustCompile("^clip-(\\d+)x(\\d+)$")
-var CropRegexp = regexp.MustCompile("^crop-(\\d+)x(\\d+)(-x(\\d+)y(\\d+))?$")
-var CutRegexp = regexp.MustCompile("^cut-(\\d+)x(\\d+)-t(\\d+)l(\\d+)(-s(\\d+)x(\\d+))?$")
+const DefaultQuality = 80
 
-type Operation interface {
+type Transformation interface {
+	Name() string
 	Apply(image.Image) (image.Image, error)
 }
 
@@ -27,6 +24,12 @@ type Format interface {
 	Save(image.Image, io.Writer) error
 	Suffix() string
 }
+
+type Operation struct {
+	T Transformation
+	F Format
+}
+
 
 type jpegFormat jpeg.Options
 
@@ -49,16 +52,48 @@ func (p pngFormat) Suffix() string {
 	return ".png"
 }
 
-func ParseOperation(format string) (Operation, error) {
+
+var SuffixRegexp = regexp.MustCompile("\\.((q(\\d+)\\.)?jpe?g|png)$")
+var ScaleRegexp = regexp.MustCompile("^(scale-)?(\\d+)x(\\d+)$")
+var ClipRegexp = regexp.MustCompile("^clip-(\\d+)x(\\d+)$")
+var CropRegexp = regexp.MustCompile("^crop-(\\d+)x(\\d+)(-x(\\d+)y(\\d+))?$")
+var CutRegexp = regexp.MustCompile("^cut-(\\d+)x(\\d+)-t(\\d+)l(\\d+)(-s(\\d+)x(\\d+))?$")
+
+
+
+func ParseOperation (format string) (*Operation, error) {
+	var tr Transformation
+	var ft Format
+	if parts := SuffixRegexp.FindStringSubmatch(format); parts != nil  {
+		if len(parts[3]) > 0 { // we have a number, must be a jpeg
+			quality, _ := strconv.Atoi(parts[3]) 
+			ft  = jpegFormat(jpeg.Options{Quality: clip(0, 100, quality)})
+		} else if parts[1] == "jpeg" {
+			ft = jpegFormat(jpeg.Options{Quality: DefaultQuality})
+		} else {
+			ft = pngFormat{}
+		}
+		format = format[0:len(format)-len(parts[0])]
+	} else {
+		ft = jpegFormat(jpeg.Options{Quality: DefaultQuality})
+	}
+	tr, err := ParseTransformation(format)
+	if err != nil {
+		return nil, err
+	}
+	return &Operation{T: tr, F: ft}, nil
+}
+
+func ParseTransformation(format string) (Transformation, error) {
 	if parts := ScaleRegexp.FindStringSubmatch(format); parts != nil {
 		width, _ := strconv.Atoi(parts[2])
 		height, _ := strconv.Atoi(parts[3])
-		return scaleOperation{width, height}, nil
+		return scaleTransformation{width, height}, nil
 	}
 	if parts := ClipRegexp.FindStringSubmatch(format); parts != nil {
 		width, _ := strconv.Atoi(parts[1])
 		height, _ := strconv.Atoi(parts[2])
-		return clipOperation{width, height}, nil
+		return clipTransformation{width, height}, nil
 	}
 	if parts := CropRegexp.FindStringSubmatch(format); parts != nil {
 		width, _ := strconv.Atoi(parts[1])
@@ -68,7 +103,7 @@ func ParseOperation(format string) (Operation, error) {
 			x, _ = strconv.Atoi(parts[4])
 			y, _ = strconv.Atoi(parts[5])
 		}
-		return cropOperation{width, height, x, y}, nil
+		return cropTransformation{width, height, x, y}, nil
 	}
 	if parts := CutRegexp.FindStringSubmatch(format); parts != nil {
 		width, _ := strconv.Atoi(parts[1])
@@ -80,7 +115,7 @@ func ParseOperation(format string) (Operation, error) {
 			sX, _ = strconv.Atoi(parts[6])
 			sY, _ = strconv.Atoi(parts[7])
 		}
-		return cutOperation{width, height, left, top, sX, sY}, nil
+		return cutTransformation{width, height, left, top, sX, sY}, nil
 	}
 	return nil, errors.New("cannot parse format")
 }
@@ -103,11 +138,15 @@ func clip(minimum, maximum, vl int) int {
 	return max(minimum, min(maximum, vl))
 }
 
-type scaleOperation struct {
+type scaleTransformation struct {
 	width, height int
 }
 
-func (s scaleOperation) Apply(in image.Image) (image.Image, error) {
+func (s scaleTransformation) Name() string {
+	return fmt.Sprintf("scale-%dx%d", s.width, s.height)
+}
+
+func (s scaleTransformation) Apply(in image.Image) (image.Image, error) {
 	out := image.NewRGBA(image.Rect(0, 0, s.width, s.height))
 	if err := graphics.Scale(out, in); err != nil {
 		return nil, err
@@ -115,12 +154,16 @@ func (s scaleOperation) Apply(in image.Image) (image.Image, error) {
 	return out, nil
 }
 
-type cropOperation struct {
+type cropTransformation struct {
 	width, height int
 	x, y          int // center points, as a percentage
 }
 
-func (s cropOperation) Apply(in image.Image) (image.Image, error) {
+func (s cropTransformation) Name() string {
+	return fmt.Sprintf("crop-%dx%d-x%dy%d", s.width, s.height, s.x, s.y)
+}
+
+func (s cropTransformation) Apply(in image.Image) (image.Image, error) {
 	// crop first scales to the maximum size
 	wOut := (s.height * in.Bounds().Dx()) / in.Bounds().Dy()
 	hOut := (s.width * in.Bounds().Dy()) / in.Bounds().Dx()
@@ -149,11 +192,15 @@ func (s cropOperation) Apply(in image.Image) (image.Image, error) {
 	return out, nil
 }
 
-type clipOperation struct {
+type clipTransformation struct {
 	width, height int
 }
 
-func (s clipOperation) Apply(in image.Image) (image.Image, error) {
+func (s clipTransformation) Name() string {
+	return fmt.Sprintf("clip-%dx%d", s.width, s.height)
+}
+
+func (s clipTransformation) Apply(in image.Image) (image.Image, error) {
 	// clip keeps aspect ratio intact, thus wOut / hOut = wIn / hIn
 	// thous wOut = wIn * hOut / hIn
 	wOut := (s.height * in.Bounds().Dx()) / in.Bounds().Dy()
@@ -174,13 +221,17 @@ func (s clipOperation) Apply(in image.Image) (image.Image, error) {
 	return out, nil
 }
 
-type cutOperation struct {
+type cutTransformation struct {
 	width, height           int
 	left, top               int
 	scaleWidth, scaleHeight int
 }
 
-func (s cutOperation) Apply(in image.Image) (image.Image, error) {
+func (s cutTransformation) Name() string {
+	return fmt.Sprintf("cut-%dx%d-t%dl%d-s%dx%d", s.width, s.height, s.top, s.left, s.scaleWidth, s.scaleHeight)
+}
+
+func (s cutTransformation) Apply(in image.Image) (image.Image, error) {
 	tmp := image.NewRGBA(image.Rect(0, 0, s.width, s.height))
 	draw.Draw(tmp, tmp.Bounds(), in, image.Pt(s.top, s.left), draw.Src)
 	if s.width == s.scaleWidth && s.height == s.scaleHeight {
@@ -192,3 +243,4 @@ func (s cutOperation) Apply(in image.Image) (image.Image, error) {
 	}
 	return out, nil
 }
+
